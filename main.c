d0c7356bf75792b5c2351dae8b4bcc94b4fe4756
Lugmaner <luki.heel@gmail.com>
2026-03-30 16:41:37 +0200

    refactor: split rocket constants and state into separate structs and update functions

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 
 typedef struct
 {
-    double currentThrust;
-    double currentAcceleration;
-    double currentVelocity;
-    double currentHeight;
-    double currentMass;
     double dryMass;
     double massFlow;
-    double dragCoefficient;
-    double currentAeroDrag;
     double crossSectionArea;
-    double remainingFuel;
-    int isFuelEmpty;
-}Rocket_t;
+    double dragCoefficient;
+}Constants_t;
+
+typedef struct
+{
+    double height;
+    double vel;
+    double thrust;
+    double mass;
+}State_t;
 
 double calc_density(double height){ //kg/m^3
     double seaDensity = 1.225;
@@ -26,90 +26,74 @@ double calc_density(double height){ //kg/m^3
     return seaDensity * exp(-height/scaleHeight);
 }
 
-int update_aerodrag(Rocket_t *rocket){
-    if(!rocket){return 1;}
-    double density = calc_density(rocket->currentHeight);
-    double v = rocket->currentVelocity;
-    double aeroDrag = 0.5 * density * v * v * rocket->dragCoefficient * rocket->crossSectionArea;
-    rocket->currentAeroDrag = v < 0 ? -aeroDrag : aeroDrag;
-    return 0;
+double calc_aerodrag(const Constants_t *constants,const State_t *state){ // newton
+    double density = calc_density(state->height);
+    double v = state->vel;
+    double aeroDrag = 0.5 * density * v * v * constants->dragCoefficient * constants->crossSectionArea;
+    return v < 0 ? -aeroDrag : aeroDrag;
 }
 
-int update_mass(Rocket_t *rocket, double dt){
-    if(!rocket){return 1;}
-    if(!rocket->isFuelEmpty){
-        rocket->currentMass -= rocket-> massFlow * dt;
+void update_mass(const Constants_t *constants,State_t *state, double dt){
+    if(state->mass <= constants->dryMass){
+        state->mass = constants->dryMass;
+        state->thrust = 0;
+        return;
     }
-    return 0;
+    state->mass -= constants-> massFlow * dt;
 }
 
-int update_acceleration(Rocket_t *rocket){
-    if(!rocket){return 1;}
-    rocket->currentAcceleration = (rocket->currentThrust / rocket->currentMass) - (rocket->currentAeroDrag / rocket->currentMass) - EARTH_GRAV;
-    return 0;
+double calc_acceleration(const Constants_t *constants,const State_t *state){ // m/s^2
+    double aeroDrag = calc_aerodrag(constants, state);
+    return (state->thrust / state->mass) - (aeroDrag / state->mass) - EARTH_GRAV;
 }
 
-int update_velocity(Rocket_t *rocket, double dt){
-    if(!rocket){return 1;}
-    rocket->currentVelocity += rocket->currentAcceleration * dt;
-    return 0;
+void update_velocity(const Constants_t *constants, State_t *state, double dt){
+    double acceleration = calc_acceleration(constants, state);
+    state->vel += acceleration * dt;
 }
 
-int update_height(Rocket_t *rocket, double dt){
-    if(!rocket){return 1;}
-    rocket->currentHeight += rocket->currentVelocity * dt;
-    if(rocket->currentHeight < 0.0){
-        rocket->currentHeight = 0.0;    // earthContact approximated
+void update_height(State_t *state, double dt){
+    state->height += state->vel * dt;
+    if(state->height < 0.0){
+        state->height = 0.0;    // earthContact approximated
     }
+}
+
+void update_physics(const Constants_t *constants, State_t *state, double dt){
+    update_mass(constants, state ,dt);
+    update_velocity(constants, state,dt);
+    update_height(state, dt);;
+}
+
+int init_simulation(Constants_t *constants, State_t *state){
+    if(!constants || !state){return 1;}
+    constants->dryMass = 25000.0; // kg
+    constants->massFlow = 12500.0; // kg/s
+    constants->crossSectionArea = 10.0; //m^2
+    constants->dragCoefficient = 0.5;
+    state->thrust = 7607000.0; // newton
+    state->vel = 0.0; // m/s
+    state->mass = 433000.0; // kg
+    state->height = 0.0; // m
     return 0;
 }
 
-int remaining_fuel(Rocket_t *rocket){
-    if(!rocket){return 1;}
-    if(rocket->currentMass <= rocket->dryMass){
-        if(!rocket->isFuelEmpty){
-            rocket->isFuelEmpty = 1;
-            rocket->remainingFuel = 0.0;
-            rocket->currentThrust = 0.0;
-            rocket->currentMass = rocket->dryMass;
-            printf("WARNING: Out of fuel!!\n");
-        }
-        return 0;
-    }
-    rocket->remainingFuel = rocket->currentMass - rocket->dryMass;
-    return 0;
-}
-
-int update_physics(Rocket_t *rocket, double dt){
-    if(update_mass(rocket,dt) == 1){return 1;}
-    if(remaining_fuel(rocket) == 1){return 1;}
-    if(update_aerodrag(rocket)== 1){return 1;}
-    if(update_acceleration(rocket) == 1){return 1;}
-    if(update_velocity(rocket,dt) == 1){return 1;}
-    if(update_height(rocket,dt) == 1){return 1;}
-    return 0;
-}
-
-int init_rocket(Rocket_t *rocket){
-    if(!rocket){return 1;}
-    rocket->currentThrust = 7607000.0; // newton
-    rocket->currentAcceleration = 0.0; // m/s^2
-    rocket->currentVelocity = 0.0; // m/s
-    rocket->currentMass = 433000.0; // kg
-    rocket->dryMass = 25000.0; // kg
-    rocket->massFlow = 12500.0; // kg/s
-    rocket->remainingFuel = rocket->currentMass - rocket->dryMass; // kg
-    rocket->currentHeight = 0.0; // m
-    rocket->crossSectionArea = 10.0; //m^2
-    rocket->dragCoefficient = 0.5;
-    rocket->currentAeroDrag = 0.0; // newton
-    rocket->isFuelEmpty = 0;
-    return 0;
+void print_flightInfo(const State_t *state, double cTime, double a, double remFuel, double aeroDrag, double density){
+    printf("Time:%.3fs | acceleration:%.3fm/s^2 | Height:%.3fm | velocity:%.3fm/s | mass:%.3fkg | remaining fuel:%.3fkg | aerodrag:%.3fN | airdensity:%.3fkg/m^3\n",
+        cTime,
+        a,
+        state->height,
+        state->vel,
+        state->mass,
+        remFuel,
+        aeroDrag,
+        density);
 }
 
 int simulate_flight(double time_limit){
-    Rocket_t newRocket;
-    if(init_rocket(&newRocket) == 1){
+    Constants_t constants;
+    State_t state;
+    if(init_simulation(&constants, &state) == 1){
         return 1;
     }
     double cTime = 0.0; // s
@@ -117,33 +101,28 @@ int simulate_flight(double time_limit){
     double maxHeight = 0.0;
     double maxVelocity = 0.0;
     double lowestDensityAt = 0.0;
-    double lowestDensity = calc_density(newRocket.currentHeight);
+    double lowestDensity = calc_density(state.height);
 
     while (cTime < time_limit)
     {
-        if(update_physics(&newRocket, dTime) == 1){return 1;}
-        if(newRocket.currentHeight > maxHeight){
-            maxHeight = newRocket.currentHeight;
-        }
-        if(fabs(newRocket.currentVelocity) > maxVelocity){
-            maxVelocity = fabs(newRocket.currentVelocity);
-        }
-        double density = calc_density(newRocket.currentHeight);
-        if(density <= lowestDensity && newRocket.currentHeight != 0.0){
-            lowestDensity = density;
-            lowestDensityAt = newRocket.currentHeight;
-        }
-        printf("Time:%.3fs | acceleration:%.3fm/s^2 | Height:%.3fm | velocity:%.3fm/s | mass:%.3fkg | remaining fuel:%.3fkg | aerodrag:%.3fN | airdensity:%.3fkg/m^3\n",
-        cTime,
-        newRocket.currentAcceleration,
-        newRocket.currentHeight,
-        newRocket.currentVelocity,
-        newRocket.currentMass,
-        newRocket.remainingFuel,
-        newRocket.currentAeroDrag,
-        density);
+        update_physics(&constants, &state, dTime);
 
-        if(newRocket.currentHeight <= 0.0 && cTime > 0.0 && newRocket.currentVelocity < 0){
+        if(state.height > maxHeight){maxHeight = state.height;}
+        if(fabs(state.vel) > maxVelocity){maxVelocity = fabs(state.vel);}
+
+        double density = calc_density(state.height);
+        double acceleration = calc_acceleration(&constants, &state);
+        double aeroDrag = calc_aerodrag(&constants, &state);
+        double remainingFuel = state.mass - constants.dryMass;
+        if(remainingFuel <= 0){remainingFuel = 0;}
+        if(density <= lowestDensity && state.height != 0.0){
+            lowestDensity = density;
+            lowestDensityAt = state.height;
+        }
+
+        print_flightInfo(&state, cTime, acceleration, remainingFuel,aeroDrag, density);
+
+        if(state.height <= 0.0 && cTime > 0.0 && state.vel < 0){
             printf("Rocket hit the ground!!");
             break;
         }
